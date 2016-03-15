ee039342913005cd0d7f07d8566f99059438bccc
Amber Ward and Priya Vasudevan <satipriya99@gmail.com>
2016-03-14 19:49:30 -0700

    add all files

---
+++ js/scripts.js
@@ -0,0 +1,38 @@
+// Business logic
+function Contact(first, last, email, phone) {
+  this.firstName = first;
+  this.lastName = last;
+  this.emailAddress = email;
+  this.phoneNumber = phone;
+}
+
+// user interface logic
+$(document).ready(function() {
+  $("form#new-contact").submit(function(event) {
+    event.preventDefault();
+
+    var inputtedFirstName = $("input#new-first-name").val();
+    var inputtedLastName = $("input#new-last-name").val();
+    var inputtedEmailAddress = $("input#new-email-address").val();
+    var inputtedPhoneNumber = $("input#new-phone-number").val();
+
+    var newContact = new Contact(inputtedFirstName, inputtedLastName, inputtedEmailAddress, inputtedPhoneNumber);
+
+    $("ul#contacts").append("<li><span class='contact'>" + newContact.firstName + "</span></li>");
+
+    $("input#new-first-name").val("");
+    $("input#new-last-name").val("");
+    $("input#new-email-address").val("");
+    $("input#new-phone-number").val("");
+
+    $(".contact").last().click(function(){
+      $("#show-contact").show();
+      $("#show-contact h2").text(newContact.firstName);
+      $(".first-name").text(newContact.firstName);
+      $(".last-name").text(newContact.lastName);
+      $(".email-address").text(newContact.emailAddress);
+      $(".phone-number").text(newContact.phoneNumber);
+    });
+
+  });
+});
