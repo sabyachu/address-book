ae3de7aefed4a06fa746ea4eb1ce0db9fb1527b7
Amanda Daly and Priya Vasudevan <techdaly@gmail.com>
2016-03-21 18:35:44 -0700

    Add prototype for full address and event listeneners

--- js/scripts.js
+++ js/scripts.js
@@ -17,8 +17,28 @@ Contact.prototype.fullName = function() {
   return this.firstName + " " + this.lastName;
 }
 
+Address.prototype.fullAddress = function() {
+  return this.street + ", " + this.city + ", " + this.state;
+}
+
 // user interface logic
 $(document).ready(function() {
+  $("#add-address").click(function() {
+  $("#new-addresses").append('<div class="new-address">' +
+                               '<div class="form-group">' +
+                                 '<label for="new-street">Street</label>' +
+                                 '<input type="text" class="form-control new-street">' +
+                               '</div>' +
+                               '<div class="form-group">' +
+                                 '<label for="new-city">City</label>' +
+                                 '<input type="text" class="form-control new-city">' +
+                               '</div>' +
+                               '<div class="form-group">' +
+                                 '<label for="new-state">State</label>' +
+                                 '<input type="text" class="form-control new-state">' +
+                               '</div>' +
+                             '</div>');
+});
   $("form#new-contact").submit(function(event) {
     event.preventDefault();
 
@@ -29,12 +49,16 @@ $(document).ready(function() {
 
     var newContact = new Contact(inputtedFirstName, inputtedLastName, inputtedEmailAddress, inputtedPhoneNumber);
 
+    $(".new-address").each(function() {
+      var inputtedStreet = $(this).find("input.new-street").val();
+      var inputtedCity = $(this).find("input.new-city").val();
+      var inputtedState = $(this).find("input.new-state").val();
+      var newAddress = new Address(inputtedStreet, inputtedCity, inputtedState);
+      newContact.addresses.push(newAddress);
+    });
+
     $("ul#contacts").append("<li><span class='contact'>" + newContact.fullName() + "</span></li>");
 
-    $("input#new-first-name").val("");
-    $("input#new-last-name").val("");
-    $("input#new-email-address").val("");
-    $("input#new-phone-number").val("");
 
     $(".contact").last().click(function(){
       $("#show-contact").show();
@@ -43,7 +67,15 @@ $(document).ready(function() {
       $(".last-name").text(newContact.lastName);
       $(".email-address").text(newContact.emailAddress);
       $(".phone-number").text(newContact.phoneNumber);
-    });
+      $("ul#addresses").text("");
+        newContact.addresses.forEach(function(address) {
+          $("ul#addresses").append("<li>" + address.fullAddress() + "</li>");
+        });
+      });
 
+      $("input#new-first-name").val("");
+      $("input#new-last-name").val("");
+      $("input#new-email-address").val("");
+      $("input#new-phone-number").val("");
   });
 });
